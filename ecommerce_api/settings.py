7a028fe184e4678278197146bf08af370b437aeb
kaidenvlr <jzxcvblessed@gmail.com>
2023-05-02 20:24:58 +0600

    updated settings

--- ecommerce_api/settings.py
+++ ecommerce_api/settings.py
@@ -144,3 +144,12 @@ MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+# Rest Framework
+
+REST_FRAMEWORK = {
+    'DEFAULT_AUTHENTICATION_CLASSES': [
+        'rest_framework.authentication.BasicAuthentication',
+        'rest_framework.authentication.SessionAuthentication'
+    ]
+}
