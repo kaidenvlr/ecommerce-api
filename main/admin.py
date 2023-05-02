ec5e04714254db1547f955bdfe78952b32c1f2d6
kaidenvlr <jzxcvblessed@gmail.com>
2023-05-02 13:52:19 +0600

    registered inline buyer instance

--- main/admin.py
+++ main/admin.py
@@ -1,3 +1,20 @@
 from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin as BaseUserAdmin
+from django.contrib.auth.models import User
 
-# Register your models here.
+from main.models import Buyer
+
+
+class BuyerInline(admin.StackedInline):
+    model = Buyer
+    can_delete = False
+    verbose_name = 'Покупатель'
+    verbose_name_plural = 'Покупатели'
+
+
+class UserAdmin(BaseUserAdmin):
+    inlines = (BuyerInline,)
+
+
+admin.site.unregister(User)
+admin.site.register(User, UserAdmin)
