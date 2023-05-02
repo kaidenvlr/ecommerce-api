8f9ca247836cc5635a737ebbc3d454b4571f1af2
kaidenvlr <jzxcvblessed@gmail.com>
2023-05-02 13:44:38 +0600

    main app models

--- main/models.py
+++ main/models.py
@@ -1,3 +1,15 @@
+from django.contrib.auth.models import User
 from django.db import models
 
-# Create your models here.
+
+class Buyer(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    number = models.CharField(max_length=200, null=True, blank=True)
+    avatar = models.ImageField(upload_to='media/avatar', null=True, blank=True)
+
+    def __str__(self):
+        return self.user.username
+
+    class Meta:
+        verbose_name = 'Покупатель'
+        verbose_name_plural = 'Покупатели'
