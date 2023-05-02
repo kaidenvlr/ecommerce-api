4d01363f89bb75fd147e6f386bd7de499415c861
kaidenvlr <jzxcvblessed@gmail.com>
2023-05-03 01:42:43 +0600

    change password view

--- main/views.py
+++ main/views.py
@@ -1,12 +1,13 @@
 from django.contrib.auth import authenticate
+from django.contrib.auth.models import User
 from django.views.decorators.csrf import csrf_exempt
 from rest_framework import status, generics
 from rest_framework.authtoken.models import Token
 from rest_framework.decorators import api_view, permission_classes
-from rest_framework.permissions import AllowAny
+from rest_framework.permissions import AllowAny, IsAuthenticated
 from rest_framework.response import Response
 
-from main.serializers import RegisterSerializer
+from main.serializers import RegisterSerializer, ChangePasswordSerializer
 
 
 # Login
@@ -29,3 +30,36 @@ def login(request):
 class RegisterUserAPIView(generics.CreateAPIView):
     permission_classes = (AllowAny,)
     serializer_class = RegisterSerializer
+
+
+class ChangeUserPasswordView(generics.UpdateAPIView):
+    model = User
+    serializer_class = ChangePasswordSerializer
+    permission_classes = (IsAuthenticated,)
+
+    def get_object(self, queryset=None):
+        user = self.request.user
+        return user
+
+    def update(self, request, *args, **kwargs):
+        self.user = self.get_object()
+        serializer = self.get_serializer(data=request.data)
+
+        if serializer.is_valid():
+            if not self.user.check_password(serializer.data.get('old_password')):
+                return Response({'error': 'Wrong password'}, status=status.HTTP_400_BAD_REQUEST)
+            if serializer.data.get('new_password') != serializer.data.get('new_password_2'):
+                return Response({'error': "Passwords didn't match"}, status=status.HTTP_400_BAD_REQUEST)
+
+            self.user.set_password(serializer.data.get("new_password"))
+            self.user.save()
+
+            response = {
+                'status': 200,
+                'code': status.HTTP_200_OK,
+                'message': 'Password updated successfully',
+                'data': []
+            }
+            return Response(response, status=status.HTTP_200_OK)
+
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
