2cc9f0861b5aa21ee7c781423ca864cc8168571f
kaidenvlr <jzxcvblessed@gmail.com>
2023-05-03 01:42:36 +0600

    change password endpoint

--- main/urls.py
+++ main/urls.py
@@ -7,5 +7,6 @@ urlpatterns = [
     # User
     path('login/', views.login, name='login'),
     path('register/', views.RegisterUserAPIView.as_view(), name='register'),
+    path('change-password/', views.ChangeUserPasswordView.as_view(), name='change-password'),
 
 ]
