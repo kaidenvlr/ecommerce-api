fd05235fbf7c9b89e13f968a9c8a6a2e5c5c9ed7
kaidenvlr <jzxcvblessed@gmail.com>
2023-05-03 23:07:25 +0600

    category endpoints

--- store/urls.py
+++ store/urls.py
@@ -1,1 +1,14 @@
-urlpatterns = []
+from django.urls import path
+
+from store.views import category
+
+urlpatterns = [
+    # Category
+    path('add-category', category.add_category, name='add-category'),
+    path('get-category', category.get_one_category, name='get-category'),
+    path('get-categories', category.get_all_categories, name='get-categories'),
+    path('update-category', category.update_category, name='update-category'),
+    path('delete-category', category.delete_category, name='delete-category'),
+
+
+]
