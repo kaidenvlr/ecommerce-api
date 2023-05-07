244ab0d15a4cd6ff201c1d63b30f5ac5f8453b2b
kaidenvlr <jzxcvblessed@gmail.com>
2023-05-07 23:10:51 +0600

    product api views

---
+++ store/views/product.py
@@ -0,0 +1,77 @@
+from django.views.decorators.csrf import csrf_exempt
+from rest_framework import status, permissions
+from rest_framework.decorators import api_view, permission_classes
+from rest_framework.response import Response
+
+from store.models import Subcategory, Category, Brand, Product
+from store.serializers import ProductSerializer
+
+
+@csrf_exempt
+@api_view(('POST',))
+@permission_classes((permissions.IsAdminUser,))
+def add_product(request):
+    product_serializer = ProductSerializer(data=request.data)
+    if product_serializer.is_valid():
+        product_serializer.save()
+
+        subcategory = Subcategory.objects.get(id=product_serializer.data.get('subcategory'))
+        subcategory.active = True
+        subcategory.save()
+
+        subcategory.category.active = True
+        subcategory.category.save()
+
+        brand = Brand.objects.get(id=product_serializer.data.get('brand'))
+        brand.active = True
+        brand.save()
+
+        return Response(product_serializer.data, status=status.HTTP_201_CREATED)
+    return Response(product_serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+
+@api_view(('GET',))
+@permission_classes((permissions.AllowAny,))
+def get_one_product(request):
+    try:
+        product = Product.objects.get(id=request.query_params.get('id'))
+    except Exception as error:
+        return Response({'error': error}, status=status.HTTP_404_NOT_FOUND)
+
+    serializer = ProductSerializer(product)
+    return Response(serializer.data, status=status.HTTP_200_OK)
+
+
+@api_view(('GET',))
+@permission_classes((permissions.AllowAny,))
+def get_all_products(request):
+    products = Product.objects.all()
+    serializer = ProductSerializer(products, many=True)
+    return Response(serializer.data, status=status.HTTP_200_OK)
+
+
+@api_view(('PUT',))
+@permission_classes((permissions.IsAdminUser,))
+def update_product(request):
+    try:
+        product = Product.objects.get(id=request.data.get('id'))
+    except Exception as error:
+        return Response({'error': error}, status=status.HTTP_404_NOT_FOUND)
+
+    serializer = ProductSerializer(product, data=request.data)
+    if serializer.is_valid():
+        serializer.save()
+        return Response(serializer.data, status=status.HTTP_202_ACCEPTED)
+    return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+
+@api_view(('DELETE',))
+@permission_classes((permissions.IsAdminUser,))
+def delete_product(request):
+    try:
+        product = Product.objects.get(id=request.data.get('id'))
+    except Exception as error:
+        return Response({'error': error}, status=status.HTTP_404_NOT_FOUND)
+
+    product.delete()
+    return Response(status=status.HTTP_204_NO_CONTENT)
