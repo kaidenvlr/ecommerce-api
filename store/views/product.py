7b89b5ce80628039bc322dc07fc2c74d62fc3555
kaidenvlr <jzxcvblessed@gmail.com>
2023-05-08 16:39:00 +0600

    new views to product image

--- store/views/product.py
+++ store/views/product.py
@@ -4,7 +4,7 @@ from rest_framework.decorators import api_view, permission_classes
 from rest_framework.response import Response
 
 from store.models import Subcategory, Category, Brand, Product
-from store.serializers import ProductSerializer
+from store.serializers import ProductSerializer, ProductImageSerializer
 
 
 @csrf_exempt
@@ -75,3 +75,26 @@ def delete_product(request):
 
     product.delete()
     return Response(status=status.HTTP_204_NO_CONTENT)
+
+
+@csrf_exempt
+@api_view(('POST',))
+@permission_classes((permissions.IsAdminUser,))
+def add_product_image(request):
+    image_serializer = ProductImageSerializer(data=request.data)
+    if image_serializer.is_valid():
+        image_serializer.save()
+        return Response(image_serializer.data, status=status.HTTP_201_CREATED)
+    return Response(image_serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+
+@api_view(('DELETE',))
+@permission_classes((permissions.IsAdminUser,))
+def delete_product_images(request):
+    try:
+        product = Product.objects.get(id=request.data.get('id'))
+        print(*product.image)
+    except Exception as error:
+        return Response({'error': error}, status=status.HTTP_404_NOT_FOUND)
+
+    return Response({'message': 'check server console'}, status=status.HTTP_200_OK)
